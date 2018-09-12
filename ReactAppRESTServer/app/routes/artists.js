dedf53be33fd0151cb6224c2d22855295ab0f257
Andrey Romanov <steampart@gmail.com>
2018-09-12 09:28:22 +0300

    Refactoring

--- ReactAppRESTServer/app/routes/artists.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const artistController = require('../controllers/artists');
-
-router.get('/', artistController.getAllArtists);
-router.post('/', artistController.addArtist);
-
-module.exports = router;
--- ReactAppRESTServer/app/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/users');
-
-router.post('/register', userController.saveUser);
-router.post('/authenticate', userController.authenticate);
-
-module.exports = router;
